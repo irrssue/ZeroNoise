260b61fc8bb59fee06aa11cce286213692a35df8
irrssue <irrssue@gmail.com>
2025-10-12 00:26:29 -0400

    adding a basic layout

--- ZeroNoise/ContentView.swift
+++ ZeroNoise/ContentView.swift
@@ -4,10 +4,11 @@
 //
 //  Created by Saw Thura Zaw on 10/11/25.
 //
-
 import SwiftUI
 
-struct PomodoroTimerView: View {
+// ZeroNoise - Focus Timer App
+
+struct ZeroNoiseTimerView: View {
     @State private var timeRemaining: Int = 25 * 60 // 25 minutes in seconds
     @State private var isActive: Bool = false
     @State private var timer: Timer?
@@ -30,50 +31,35 @@ struct PomodoroTimerView: View {
             Color(red: 0.95, green: 0.95, blue: 0.95)
                 .ignoresSafeArea()
             
-            VStack {
-                Spacer()
-                
-                // Timer Display
-                ZStack {
-                    // Red progress circle (dashed)
-                    Circle()
-                        .trim(from: 0, to: progress)
-                        .stroke(
-                            Color.red,
-                            style: StrokeStyle(
-                                lineWidth: 8,
-                                lineCap: .round,
-                                dash: [15, 10]
-                            )
+            // Timer Display - Tappable area
+            ZStack {
+                // Red progress circle (dashed)
+                Circle()
+                    .trim(from: 0, to: progress)
+                    .stroke(
+                        Color.red,
+                        style: StrokeStyle(
+                            lineWidth: 8,
+                            lineCap: .round,
+                            dash: [15, 10]
                         )
-                        .frame(width: 280, height: 280)
-                        .rotationEffect(.degrees(-90))
-                        .animation(.linear(duration: 1), value: progress)
-                    
-                    // Black inner circle
-                    Circle()
-                        .stroke(Color.black, lineWidth: 8)
-                        .frame(width: 280, height: 280)
-                    
-                    // Time text
-                    Text(timeString)
-                        .font(.system(size: 72, weight: .regular, design: .default))
-                        .foregroundColor(.black)
-                }
+                    )
+                    .frame(width: 280, height: 280)
+                    .rotationEffect(.degrees(-90))
+                    .animation(.linear(duration: 1), value: progress)
                 
-                Spacer()
+                // Black inner circle
+                Circle()
+                    .stroke(Color.black, lineWidth: 8)
+                    .frame(width: 280, height: 280)
                 
-                // Start/Pause button
-                Button(action: toggleTimer) {
-                    Circle()
-                        .fill(Color.white)
-                        .frame(width: 80, height: 80)
-                        .overlay(
-                            Circle()
-                                .stroke(Color.black, lineWidth: 4)
-                        )
-                }
-                .padding(.bottom, 60)
+                // Time text
+                Text(timeString)
+                    .font(.system(size: 72, weight: .regular, design: .default))
+                    .foregroundColor(.black)
+            }
+            .onTapGesture {
+                toggleTimer()
             }
         }
     }
@@ -107,8 +93,8 @@ struct PomodoroTimerView: View {
 }
 
 // Preview
-struct PomodoroTimerView_Previews: PreviewProvider {
+struct ZeroNoiseTimerView_Previews: PreviewProvider {
     static var previews: some View {
-        PomodoroTimerView()
+        ZeroNoiseTimerView()
     }
 }
